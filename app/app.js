42e8e255dfc9d180e1c8f624e7148cdbe354f939
opuerto <oscar.puertohn@gmail.com>
2016-10-04 11:47:29 +0000

    wihitelist jwt

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
 /*app*/
 var mercurioApp = angular.module('mercurioApp', ['ngRoute', 'ngResource','ngCookies',
-	'ngSanitize','ngAnimate','ngStorage','angular-jwt','auth0','angular-loading-bar']);
+	'ngSanitize','ngAnimate','ngStorage','angular-jwt','auth0','angular-loading-bar'])
+.config(function Config($httpProvider, jwtOptionsProvider){
+	jwtOptionsProvider.config({
+		whiteListedDomains: ['app.mercurio.hn']
+	});
+});
